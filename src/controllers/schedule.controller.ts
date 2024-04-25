f9f2b6bbb9dae21da6b536dfade0f807a3ea85c4
Angela <yaoliu928@gmail.com>
2024-04-25 23:08:32 +1000

    enable CRUD for schedules

--- src/controllers/schedule.controller.ts
+++ src/controllers/schedule.controller.ts
@@ -1,5 +1,13 @@
 import { prisma } from '../utils/prismaClient';
 
+const getAllSchedules = async (
+  req: unknown,
+  res: { json: (arg0: { id: string; accountId: number; agentId: number; startTime: Date; endTime: Date }[]) => void },
+) => {
+  const schedules = await prisma.schedule.findMany();
+  res.json(schedules);
+};
+
 const addSchedule = async (
   req: { body: { accountId: number; agentId: number; startTime: Date; endTime: Date } },
   res: { json: (arg0: { id: string; accountId: number; agentId: number; startTime: Date; endTime: Date }) => void },
@@ -16,4 +24,50 @@ const addSchedule = async (
   res.json(result);
 };
 
-export { addSchedule };
+const getScheduleById = async (
+  req: { params: { id: string | undefined } },
+  res: {
+    json: (arg0: { id: string; accountId: number; agentId: number; startTime: Date; endTime: Date } | null) => void;
+  },
+) => {
+  const { id } = req.params;
+
+  const schedules = await prisma.schedule.findUnique({
+    where: { id },
+  });
+  res.json(schedules);
+};
+
+const deleteScheduleById = async (
+  req: { params: { id: string | undefined } },
+  res: { json: (arg0: { id: string; accountId: number; agentId: number; startTime: Date; endTime: Date }) => void },
+) => {
+  const { id } = req.params;
+  const schedule = await prisma.schedule.delete({
+    where: {
+      id,
+    },
+  });
+  res.json(schedule);
+};
+
+const updateScheduleById = async (
+  req: { params: { id: string }; body: { accountId: number; agentId: number; startTime: Date; endTime: Date } },
+  res: { json: (arg0: { id: string; accountId: number; agentId: number; startTime: Date; endTime: Date }) => void },
+) => {
+  const { id } = req.params;
+  const { accountId, agentId, startTime, endTime } = req.body;
+
+  const schedule = await prisma.schedule.update({
+    where: { id },
+    data: {
+      accountId,
+      agentId,
+      startTime,
+      endTime,
+    },
+  });
+  res.json(schedule);
+};
+
+export { addSchedule, getAllSchedules, getScheduleById, deleteScheduleById, updateScheduleById };
--- src/routes/schedule.router.ts
+++ src/routes/schedule.router.ts
@@ -1,8 +1,18 @@
 import Router from 'express';
-import { addSchedule } from '../controllers/schedule.controller';
+import {
+  addSchedule,
+  getAllSchedules,
+  getScheduleById,
+  deleteScheduleById,
+  updateScheduleById,
+} from '../controllers/schedule.controller';
 
 const scheduleRouter = Router();
 
+scheduleRouter.get('/', getAllSchedules);
 scheduleRouter.post('/', addSchedule);
+scheduleRouter.get('/:id', getScheduleById);
+scheduleRouter.delete('/:id', deleteScheduleById);
+scheduleRouter.put('/:id', updateScheduleById);
 
 export { scheduleRouter };
