8e60e80442cc8fd6680b2a8b268cd6aa5687abf7
Angela <yaoliu928@gmail.com>
2024-04-25 19:25:59 +1000

    config env in docker compost

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import cors from 'cors';
 
 dotenv.config();
+const prisma = new PrismaClient();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +17,19 @@ app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('My Server');
+});
+
+app.post(`/schedules`, async (req, res) => {
+  const { accountId, agentId, startTime, endTime } = req.body;
+  const result = await prisma.schedule.create({
+    data: {
+      accountId,
+      agentId,
+      startTime,
+      endTime,
+    },
+  });
+  res.json(result);
 });
 
 app.listen(port, () => {
