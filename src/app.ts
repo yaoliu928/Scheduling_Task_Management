0f5b184e8755617580b70f377982100d3776f2f1
Angela <yaoliu928@gmail.com>
2024-04-27 15:56:51 +1000

    setup app.ts

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Express } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import { v1Router } from './routes';
+import { formatResponseMiddleware } from './middleware/formatResponse.middleware';
+import { pathNotFoundMiddleware } from './middleware/pathNotFound.middleware';
+import { validationErrorMiddleware } from './middleware/errorMiddleware/validationError.middleware';
+import { unknownErrorMiddleware } from './middleware/errorMiddleware/unknownError.middleware';
+import { notFoundErrorMiddleware } from './middleware/errorMiddleware/notFoundError.middleware';
+// import { morganConfig } from './common/morgan';
+
+const app: Express = express();
+
+app.use(helmet());
+app.use(cors());
+
+app.use(express.json());
+
+app.use(formatResponseMiddleware);
+
+// app.use(morganConfig);
+
+app.use('/v1', v1Router);
+
+app.use(pathNotFoundMiddleware);
+
+app.use(validationErrorMiddleware);
+app.use(notFoundErrorMiddleware);
+app.use(unknownErrorMiddleware);
+
+export { app };
